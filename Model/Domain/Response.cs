be182165e690cb525ec7638606bc3ad416afa54b
piyush790352 <mahatopiyush178@gmail.com>
2023-08-22 21:04:29 +0530

    commit changes

--- Model/Domain/Response.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DemoProject1.API.Model.Domain
-{
-    public class Response<T>
-    {
-            public T Result { get; set; }
-            public string StatusMessage { get; set; }
-            public string Error { get; set; }
-    }
-}
